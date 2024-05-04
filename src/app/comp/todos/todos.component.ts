4353011d552ac4f9a146ebfd5dd2c8290518e6b8
Param <111742883+P6s-fx@users.noreply.github.com>
2024-05-04 20:24:36 +0530

    add-todo section added

--- src/app/comp/todos/todos.component.ts
+++ src/app/comp/todos/todos.component.ts
@@ -3,12 +3,13 @@ import { Todo } from '../../models/Todo';
 // import test from 'node:test';
 import { CommonModule } from '@angular/common';
 import { TodoItemComponent } from '../todo-item/todo-item.component';
+import { AddTodoComponent } from '../add-todo/add-todo.component';
 // import { title } from 'process';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [CommonModule,TodoItemComponent],
+  imports: [CommonModule,TodoItemComponent,AddTodoComponent],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
@@ -30,11 +31,10 @@ export class TodosComponent {
       },
     ]
   }
-
+  
   deleteTodo(todo:Todo){
     console.log(todo);
     const index = this.todos.indexOf(todo);
     this.todos.splice(index,1)
   }
-
 }
